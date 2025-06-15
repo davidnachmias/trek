8f2026ae0b1613a73c64b8b6e85ed65054d75fc7
davidnachmias <123abadodi@gmail.com>
2025-06-15 19:27:42 +0300

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 import Technologies from "./components/Technologies";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
-import Contact from "./components/contact";
+import Contact from "./components/Contact";
 const App = () => {
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-600 selection:text-cyan-900">
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,5 @@
 import { CONTACT } from "../constants/constants";
-export default function contact() {
+export default function Contact() {
   return (
     <div className="border-b border-neutral-900 pb-4">
       <h2 className="my-20 text-center  text-4xl">Contact</h2>
